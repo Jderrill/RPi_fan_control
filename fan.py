0fda892bcef1294ee6f4f9fd6ea968d3a56a6ac3
jderrill <jderrill@gmail.com>
2017-06-12 02:59:48 +0000

    removed print commands

--- fan.py
+++ fan.py
@@ -8,7 +8,6 @@ import RPi.GPIO as GPIO
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 
-#GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(13, GPIO.OUT)
 GPIO.output(13,GPIO.LOW)
 
@@ -18,21 +17,16 @@ def CPUtemp():
     return temp
 
 def turnOnFan(temp):
-##    temp = CPUtemp()
     high_temp = 50.0
     
     if temp > high_temp:
         GPIO.output(13,GPIO.HIGH)
-        print temp
     else:
         GPIO.output(13,GPIO.LOW)
-        print temp
         sleep(30)
     return()
     
     
 while True:
-##    temp = raw_input("temp")
     temp = float(CPUtemp())
-##    print temp
     turnOnFan(temp)
